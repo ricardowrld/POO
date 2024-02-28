5f95fa871c04cc4caa42840158ce3fcd4fdf96fd
unknown <ti@LAB1-PC20.ifprcvel.internal>
2024-02-28 12:51:34 -0300

    .\ExercicioVariaveis.java

---
+++ ExercicioVariaveis.java
@@ -0,0 +1,15 @@
+public class ExercicioVariaveis {
+ 
+    public static void main(String[] args){
+        boolean Variavel1 = true;
+        int Variavel2 = 10;
+        long Variavel3 = 1L;
+        float Variavel4 = 5.2f;
+        double Variavel5 = 23.1;
+        char Variavel6 = 5;
+        byte Variavel7 = 1;
+        short Variavel8 = 15;
+        System.out.println(" boolean: \n" + Variavel1 + " Int: \t" + Variavel2 + " Long: " + Variavel3 + " Float: " + Variavel4 + " Double: " + Variavel5 + " Char: " + (char)Variavel6 + " Byte: " + Variavel7 + " Short: " + Variavel8);
+
+       }
+}
